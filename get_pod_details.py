fd9038f00c8bc3e28bb5aced772b329be0f3757e
Soumyadev Desktop <soumyadev.poriya@gmail.com>
2023-03-20 23:23:32 +0530

    python file changed

--- get_pod_details.py
+++ get_pod_details.py
@@ -10,13 +10,17 @@ import os
 import pint
 import kubernetes
 from kubernetes import client, config, watch
+from kubernetes.client.models import V1PodSpec
+from kubernetes.client.models import V1Container
+from kubernetes.client.models import V1Pod
+from kubernetes.client.models import V1ObjectMeta
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World'
-
 
 
 # K8 Pod Details
@@ -24,23 +28,24 @@ def hello_world():
 def get_pod_details():
     config.load_incluster_config()
     v1 = kubernetes.client.CoreV1Api()
-    #v2 = kubernetes.client.V1Pod()
-    #ret = v1.list_node()
+    # v2 = kubernetes.client.V1Pod()
+    # ret = v1.list_node()
     ret = v1.list_pod_for_all_namespaces(watch=False)
-    #details = " " + ret
+    # details = " " + ret
     details = " "
     for i in ret.items:
-        details = details + " " + i.status.pod_ip+ " " + i.metadata.namespace + " " + i.metadata.name + "\n"
+        details = details + " " + i.status.pod_ip + " " + i.metadata.namespace + " " + i.metadata.name + "\n"
 
-    return jsonify({"message":"POD Details ", "Information: ": details})
+    return jsonify({"message": "POD Details ", "Information: ": details})
+
 
 # K8 Pod Details
 @app.route('/hello/readpod')
 def get_readpod():
     config.load_incluster_config()
     v1 = kubernetes.client.CoreV1Api()
-    #podspec_obj = kubernetes.client.V1Pod()
-    #ret = v1.list_node()
+    # podspec_obj = kubernetes.client.V1Pod()
+    # ret = v1.list_node()
     name = 'dataflowpod'
     namespace = 'default'
     ret = v1.read_namespaced_pod(name, namespace)
@@ -48,7 +53,8 @@ def get_readpod():
     if ret:
         details = "Exists"
 
-    return jsonify({"message":"POD Details ", "Information: ": details})
+    return jsonify({"message": "POD Details ", "Information: ": details})
+
 
 # K8 Pod Creation
 @app.route('/hello/createpod')
@@ -81,6 +87,38 @@ def get_createpod():
         time.sleep(1)
 
     ret = v1.read_namespaced_pod(name, namespace)
+
+    if ret:
+        details = "Created"
+
+    return jsonify({"message": "POD Details ", "Information: ": details})
+
+
+# K8 Pod Creation
+@app.route('/hello/createpodsecond')
+def get_createpodsecond():
+    config.load_incluster_config()
+    v1 = kubernetes.client.CoreV1Api()
+
+    container_name = 'data-flowcontainer'
+    namespace = 'default'
+    pod_name = 'my-dataflow-pod'
+
+    image = 'gcr.io/mimetic-parity-378803/dataflow_pipeline:latest'
+    container = V1Container(name=container_name, image=image)
+    podspec = V1PodSpec(containers=[container], restart_policy="Never")
+    metadata = V1ObjectMeta(name=pod_name, namespace=namespace)
+    pod = V1Pod(api_version='v1', kind='Pod', metadata=metadata, spec=podspec)
+
+    my_pod = v1.create_namespaced_pod(namespace, pod)
+
+    while True:
+        resp = v1.read_namespaced_pod(name=pod_name, namespace='default')
+        if resp.status.phase != 'Pending':
+            break
+        time.sleep(1)
+
+    ret = v1.read_namespaced_pod(pod_name, namespace)
 
     if ret:
         details = "Created"
